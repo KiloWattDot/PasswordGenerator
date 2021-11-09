734f70978b1fb7ba6869b9950e71d0c61246a21d
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2021-11-09 16:55:34 -0500

    Added some prompts and some comments for future code

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,35 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function generatePassword() {
-var characters = "abcdefjhijklmnopqrstuvwxyz0123456789"
+// step 1. prompt user of password criteria
+// - 8 - 128 charcter password
+// - upper, lower, numeric, and special characters allowed
+  let HowMany = prompt('How many characters would you like the password to be (up to 128)?');
+    console.log(HowMany);
+    if ( 8 <= HowMany > 128) {
+        console.log('invalid');
+    }
+
+  let upper = confirm('Would you like uppercase characters in your password?');
+    console.log(upper);
+  let lower = confirm('Would you like lowercase characters in your password?');
+    console.log(lower);
+  let special = confirm('Would you like special characters in your password?');
+    console.log(special);
 
 
 
+// step 2. Validate the user chosen password contraints
 
 
+
+// step 3. Generate Pass word
+
+
+  return "Hello"
 
 }
-
+// Displays passwords
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
